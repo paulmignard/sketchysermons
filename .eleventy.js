2d2b649887b07e1b7dc81dc15e3de8d3d5a2879f
Paul Mignard <paul@mignard.co>
2020-08-12 21:02:36 -0400

    Adding new template parts, new content pages...

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,23 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setTemplateFormats([
+        "png",
+        "jpg",
+        "gif",
+        "js",
         "liquid",
         "html",
         "otf",
         "md",
         "css" // css is not yet a recognized template extension in Eleventy
     ]);
+
+    let liquidJs = require("liquidjs");
+    let options = {
+        extname: ".liquid",
+        dynamicPartials: true,
+        strict_filters: true,
+        root: ["_includes"]
+    };
+
+    eleventyConfig.setLibrary("liquid", liquidJs(options))
   };
